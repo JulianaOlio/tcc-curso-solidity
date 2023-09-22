ca1cab37dca69b97f9b6665b02ce858b45c5e0a8
JulianaOlio <115197866+JulianaOlio@users.noreply.github.com>
2023-09-21 21:15:38 -0300

    Update custodia.sol

--- custodia.sol
+++ custodia.sol
@@ -1,1 +1,30 @@
+// SPDX-License-Identifier: MIT
+
+
+pragma solidity 0.8.19;
+
+import ";
+
+contract Custodia{
+    address public owner;
+    address public enderecoToken;
+    
+    constructor(address _enderecoToken) {
+        owner = msg.sender;
+        tokenAddress = _enderecoToken;
+    }
+
+    function MeuSaldo() public view returns (uint256) {
+        IERC20 token = IERC20(enderecoToken);
+        return token.balanceOf(address(this));
+    }
+
+    function GerarTokenParaEuCliente(uint256 amount) public {
+        require(msg.sender == owner, "Somente o proprietario pode gerar tokens.");
+        
+        IERC20 token = IERC20(tokenAddress);
+        token.transfer(owner, amount);
+    }
+}
+
 
