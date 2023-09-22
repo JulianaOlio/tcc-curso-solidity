f5d9cce09d27733bcca4139f37f43fda5fcb14d8
JulianaOlio <115197866+JulianaOlio@users.noreply.github.com>
2023-09-21 22:35:48 -0300

    Update cadastro.sol

--- cadastro.sol
+++ cadastro.sol
@@ -1,1 +1,62 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
 
+// Cadastro: 
+
+contract Cadastro {
+
+    struct Cliente {
+        uint256 id;
+        string primeiroNome;
+        string sobreNome;
+        address payable endereco; //0x0
+        bytes32 hashConta; // 0x0        
+        bool existe; //false
+    }
+
+    uint256 public totalClientes;
+
+    Cliente[] public clientes;
+
+    function addCliente(
+        string memory _primeiroNome,
+        string memory _sobreNome,        
+        string memory _agencia,
+        string memory _conta
+    ) external returns (bool) {
+        string memory strTemp = string.concat(_agencia, _conta);
+        bytes memory bTemp = bytes(strTemp);
+        bytes32 hashTemp = keccak256(bTemp);
+
+        Custodia custodiaTemp = new Custodia(hashTemp);
+
+        Cliente memory cliente = Cliente(totalClientes, _primeiroNome, _sobreNome, payable(address(custodiaTemp)), hashTemp, true);
+        totalClientes++;
+        
+        clientes.push(cliente);
+        
+        return true;
+    }
+
+    function getClientePeloId(uint256 _id) external view returns (Cliente memory cliente_, bool existe) {
+        cliente_ = clientes[_id];
+        existe = cliente_.existe;
+        return (cliente_, existe);
+    }
+
+}
+
+contract Custodia {
+    bytes32 public hashConta;
+
+    event EtherRecebido();
+
+    constructor(bytes32 _hashConta) {
+        hashConta = _hashConta;
+    }
+
+    receive() external payable {
+        emit EtherRecebido();
+    }
+    
+}
